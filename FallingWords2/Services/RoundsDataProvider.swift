df1155c855341896e8f2123cbc8bdfc8565ee022
Alex Shubin <alexshubin22@gmail.com>
2020-07-26 22:56:46 +0200

    data provider test

--- FallingWords2/Services/RoundsDataProvider.swift
+++ FallingWords2/Services/RoundsDataProvider.swift
@@ -1,50 +1,55 @@
 import Foundation
 import Combine
 
-protocol RoundsDataProviderType {
-    func roundsData(roundsCount: Int) -> AnyPublisher<[RoundData], Error>
-}
-
 struct RoundsDataProvider {
-    private let translatedWordsLoader: TranslatedWordsLoaderType
+    let provide: (_ roundsCount: Int) -> AnyPublisher<[RoundData], Error>
 
-    func roundsData(roundsCount: Int) -> AnyPublisher<[RoundData], Error> {
-        translatedWordsLoader.translatedWords.map { allWords -> [RoundData] in
-            let shuffledPrefix = allWords.shuffled().prefix(roundsCount)
-            let half = roundsCount/2
-            let halfOfRoundsCorrect = shuffledPrefix.prefix(half).map {
-                return RoundData(questionWord: $0.eng,
-                                 answerWord: $0.spa,
-                                 isTranslationCorrect: true)
+    static func `default`(
+        translatedWordsLoader: TranslatedWordsLoader = .default,
+        wordsShuffler: @escaping ([TranslatedWord]) -> [TranslatedWord] = { $0.shuffled() },
+        roundsShuffler: @escaping ([RoundData]) -> [RoundData] = { $0.shuffled() }
+    ) -> RoundsDataProvider {
+        Self { roundsCount in
+            translatedWordsLoader.load().tryMap { allWords -> [RoundData] in
+                guard roundsCount <= allWords.count else { throw RoundsDataProviderError.notEnoughData }
+
+                let allWordsShuffled = wordsShuffler(allWords)
+                let shuffledPrefix = Array(allWordsShuffled.prefix(roundsCount))
+                let shuffledSuffix = Array(allWordsShuffled.suffix(roundsCount))
+
+                var result: [RoundData] = []
+
+                for i in (0..<roundsCount) {
+                    if i < roundsCount/2 {
+                        result.append(.init(questionWord: shuffledPrefix[i].eng,
+                                            answerWord: shuffledPrefix[i].spa,
+                                            isTranslationCorrect: true))
+                    } else {
+                        result.append(.init(questionWord: shuffledPrefix[i].eng,
+                                            answerWord: shuffledSuffix[i].spa,
+                                            isTranslationCorrect: shuffledSuffix[i].spa == shuffledPrefix[i].spa))
+                    }
+                }
+
+                return roundsShuffler(result)
             }
-            let halfOfRoundsCorrectOrNot: [RoundData] = shuffledPrefix.suffix(roundsCount-half).map {
-                let allPossibleTranslations = Set(allWords.map { $0.spa })
-                let answerWord = allPossibleTranslations.randomElement() ?? $0.spa
-                return RoundData(questionWord: $0.eng,
-                                 answerWord: answerWord,
-                                 isTranslationCorrect: answerWord == $0.spa)
-            }
-            return (halfOfRoundsCorrect + halfOfRoundsCorrectOrNot).shuffled()
+            .eraseToAnyPublisher()
         }
-        .eraseToAnyPublisher()
     }
 }
 
-protocol TranslatedWordsLoaderType {
-    var translatedWords: AnyPublisher<[TranslatedWord], Error> { get }
-} 
+enum RoundsDataProviderError: Error {
+    case notEnoughData
+}
 
-struct TranslatedWordsLoader: TranslatedWordsLoaderType {
-    private let url = URL(string: "https://raw.githubusercontent.com/AlexShubin/FallingWords2/master/words.json")!
+struct TranslatedWordsLoader {
+    let load: () -> AnyPublisher<[TranslatedWord], Error>
 
-    private let urlSession = URLSession.shared
-
-    private let decoder = JSONDecoder()
-
-    var translatedWords: AnyPublisher<[TranslatedWord], Error> {
-        urlSession.dataTaskPublisher(for: url)
+    static let `default` = Self {
+        let url = URL(string: "https://raw.githubusercontent.com/AlexShubin/FallingWords2/master/words.json")!
+        return URLSession.shared.dataTaskPublisher(for: url)
             .map { data, _ in data }
-            .decode(type: [TranslatedWord].self, decoder: decoder)
+            .decode(type: [TranslatedWord].self, decoder: JSONDecoder())
             .eraseToAnyPublisher()
     }
 }
---
+++ FallingWords2Tests/RoundsDataProviderTest.swift
@@ -0,0 +1,42 @@
+import XCTest
+@testable import FallingWords2
+import Combine
+
+class RoundsDataProviderTests: XCTestCase {
+    let sut = RoundsDataProvider.default(
+        translatedWordsLoader: .mock,
+        wordsShuffler: { $0 },
+        roundsShuffler: { $0 }
+    )
+
+    var cancellable: Cancellable?
+
+    func testProviderHappyPath() {
+        var result: [RoundData] = []
+        cancellable = sut.provide(2)
+            .sink(receiveCompletion: { _ in }) {
+                result = $0
+        }
+        XCTAssertEqual(result, [.init(questionWord: "1", answerWord: "1t", isTranslationCorrect: true),
+                                .init(questionWord: "2", answerWord: "4t", isTranslationCorrect: false)])
+    }
+}
+
+private extension TranslatedWordsLoader {
+    static let mock = Self {
+        .just([.init(eng: "1", spa: "1t"),
+               .init(eng: "2", spa: "2t"),
+               .init(eng: "3", spa: "3t"),
+               .init(eng: "4", spa: "4t")])
+    }
+}
+
+private extension Publisher {
+    static func just(_ output: Output) -> AnyPublisher<Output, Error> {
+        return Just(output)
+            .mapError(absurd)
+            .eraseToAnyPublisher()
+    }
+}
+
+private func absurd<A>(_ a: Never) -> A {}
