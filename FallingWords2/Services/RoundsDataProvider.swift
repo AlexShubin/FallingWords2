62a504cba3ecc72214c2fa530a9e546449163153
Alex Shubin <alexshubin22@gmail.com>
2020-07-26 11:50:14 +0200

    fix services

--- FallingWords2/Services/RoundsDataProvider.swift
+++ FallingWords2/Services/RoundsDataProvider.swift
@@ -1,74 +1,50 @@
-////
-////  pr.swift
-////  FallingWords2
-////
-////  Created by ashubin on 23.07.20.
-////  Copyright © 2020 Alex Shubin. All rights reserved.
-////
-//
-//import Foundation
-//import Combine
-//
-//protocol RoundsDataProviderType {
-//    func roundsData(roundsCount: Int) -> Future<[RoundData], Error>
-//}
-//
-//struct RoundsDataProvider {
-//
-//    private let translatedWordsLoader: TranslatedWordsLoaderType
-//
-//    func roundsData(roundsCount: Int) -> Future<[RoundData], Error> {
-//
-//        Future {
-//
-//        }
-//
-////        let x = translatedWordsLoader.translatedWords.map { allWords -> [RoundData] in
-////            let shuffledPrefix = allWords.shuffled().prefix(roundsCount)
-////            let half = roundsCount/2
-////            let halfOfRoundsCorrect = shuffledPrefix.prefix(half).map {
-////                return RoundData(questionWord: $0.eng,
-////                                 answerWord: $0.spa,
-////                                 isTranslationCorrect: true)
-////            }
-////            let halfOfRoundsCorrectOrNot: [RoundData] = shuffledPrefix.suffix(roundsCount-half).map {
-////                let allPossibleTranslations = Set(allWords.map { $0.spa })
-////                let answerWord = allPossibleTranslations.randomElement() ?? $0.spa
-////                return RoundData(questionWord: $0.eng,
-////                                 answerWord: answerWord,
-////                                 isTranslationCorrect: answerWord == $0.spa)
-////            }
-////            return (halfOfRoundsCorrect + halfOfRoundsCorrectOrNot).shuffled()
-////        }
-////        return x.eraseToAnyPublisher().
-//    }
-//}
-//
-//protocol TranslatedWordsLoaderType {
-//    var translatedWords: Future<[TranslatedWord], TranslatedWordsLoaderError> { get }
-//}
-//
-//enum TranslatedWordsLoaderError: Error {
-//    case parsingError
-//    case connectionError
-//}
-//
-//struct TranslatedWordsLoader: TranslatedWordsLoaderType {
-//    private let url = URL(string: "https://raw.githubusercontent.com/AlexShubin/FallingWords2/master/words.json")!
-//
-//    private let decoder = JSONDecoder()
-//
-//    var translatedWords: Future<[TranslatedWord], TranslatedWordsLoaderError> {
-//        Future { promise in
-//            guard let data = try? Data(contentsOf: self.url) else {
-//                promise(.failure(TranslatedWordsLoaderError.connectionError))
-//                return
-//            }
-//            guard let words = try? self.decoder.decode([TranslatedWord].self, from: data) else {
-//                promise(.failure(TranslatedWordsLoaderError.parsingError))
-//                return
-//            }
-//            promise(.success(words))
-//        }
-//    }
-//}
+import Foundation
+import Combine
+
+protocol RoundsDataProviderType {
+    func roundsData(roundsCount: Int) -> AnyPublisher<[RoundData], Error>
+}
+
+struct RoundsDataProvider {
+    private let translatedWordsLoader: TranslatedWordsLoaderType
+
+    func roundsData(roundsCount: Int) -> AnyPublisher<[RoundData], Error> {
+        translatedWordsLoader.translatedWords.map { allWords -> [RoundData] in
+            let shuffledPrefix = allWords.shuffled().prefix(roundsCount)
+            let half = roundsCount/2
+            let halfOfRoundsCorrect = shuffledPrefix.prefix(half).map {
+                return RoundData(questionWord: $0.eng,
+                                 answerWord: $0.spa,
+                                 isTranslationCorrect: true)
+            }
+            let halfOfRoundsCorrectOrNot: [RoundData] = shuffledPrefix.suffix(roundsCount-half).map {
+                let allPossibleTranslations = Set(allWords.map { $0.spa })
+                let answerWord = allPossibleTranslations.randomElement() ?? $0.spa
+                return RoundData(questionWord: $0.eng,
+                                 answerWord: answerWord,
+                                 isTranslationCorrect: answerWord == $0.spa)
+            }
+            return (halfOfRoundsCorrect + halfOfRoundsCorrectOrNot).shuffled()
+        }
+        .eraseToAnyPublisher()
+    }
+}
+
+protocol TranslatedWordsLoaderType {
+    var translatedWords: AnyPublisher<[TranslatedWord], Error> { get }
+} 
+
+struct TranslatedWordsLoader: TranslatedWordsLoaderType {
+    private let url = URL(string: "https://raw.githubusercontent.com/AlexShubin/FallingWords2/master/words.json")!
+
+    private let urlSession = URLSession.shared
+
+    private let decoder = JSONDecoder()
+
+    var translatedWords: AnyPublisher<[TranslatedWord], Error> {
+        urlSession.dataTaskPublisher(for: url)
+            .map { data, _ in data }
+            .decode(type: [TranslatedWord].self, decoder: decoder)
+            .eraseToAnyPublisher()
+    }
+}
