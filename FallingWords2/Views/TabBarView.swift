9e1624377dc5336aec160e9d9fc999c10913ca10
Alex Shubin <alexshubin22@gmail.com>
2020-07-26 12:09:29 +0200

    move image

--- FallingWords2/Views/TabBarView.swift
+++ FallingWords2/Views/TabBarView.swift
@@ -25,6 +25,11 @@ struct TabBarView: View {
     }
 }
 
+private extension Image {
+    static let game = Image(systemName: "gamecontroller.fill")
+    static let list = Image(systemName: "list.dash")
+}
+
 struct TabBarView_Previews: PreviewProvider {
     static var previews: some View {
         TabBarView(appStore: AppStore(state: AppState(), reducer: appReducer))
